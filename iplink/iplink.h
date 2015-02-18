428eed79ccef67b79ac4987dbc082c808410ed22
Timothy Yong <tyong_23@hotmail.com>
2015-02-18 00:53:00 -0500

    cpp compat added

--- iplink/iplink.h
+++ iplink/iplink.h
@@ -4,6 +4,10 @@
 #define IPLINK_GET  0x01
 #define IPLINK_POST 0x02
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct iplink {
   char *hostname;
   char ipaddr[100];
@@ -14,5 +18,9 @@ int iplink_connect_main_server(iplink_t *connection);
 int iplink_send(iplink_t *connection, char *addr, int type, char *data);
 int iplink_recv(iplink_t *connection, char *buf, int buflen);
 void iplink_disconnect(iplink_t *connection);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
