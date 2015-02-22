6e3068eaac17363902c75998ef0632c2cde21c22
Timothy Yong <tyong_23@hotmail.com>
2015-02-22 12:57:00 -0500

    simple switch

---
+++ core/README.md
@@ -0,0 +1,1 @@
+TODO test the switch for manual
---
+++ core/core.c
@@ -0,0 +1,51 @@
+/************************************
+ * The purpose of this object is to
+ * translate different formats of
+ * communication to their respective
+ * devices. You can think of it as
+ * the front end, as well as the
+ * starting program.
+ ***********************************/
+#include <signal.h>
+#include <stdint.h>
+#include <string.h>
+#include "robot.h"
+#include "manual.h"
+
+// Signal handler for killing the program
+static int stop_signal;
+void stop_program(int signum) {
+  stop_signal = 1;
+}
+
+/** This is the starting program for the robot
+ *  think of it as init.
+ */
+int main(int argc, char *argv[]) {
+  manual_t mnl;
+  robotctrl_t *ctrl;
+  int manual_mode;
+
+  // set everything
+  robot_set(TENNIS_BALL_ROBOT);
+  manual_connect(&mnl);
+  manual_mode = 1;
+
+  // start getting communication accesses
+  while (!stop_signal) {
+    // choose input
+    if (manual_mode) {
+      if ((ctrl = manual_get(&mnl))) {
+        robot_set_controls(ctrl);
+      }
+    } else {
+      
+    }
+  }
+
+  // clean up
+  manual_disonnect(&mnl);
+  robot_unset();
+
+  return 0;
+}
