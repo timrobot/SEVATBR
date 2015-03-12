aa7b03eb770f5dc78cb6f7ff1360c013830c7edd
Timothy Yong <tyong_23@hotmail.com>
2015-03-12 05:41:20 -0400

    started dev of slam

--- slam/slam.c
+++ slam/slam.c
@@ -1,24 +1,70 @@
-typedef struct polar {
-  double radius;
-  double theta;
-} polar_t;
+// using particle filters
+#include <math.h>
+#include "robot.h"
 
-typedef struct pose {
-  double x;
-  double y;
-  double theta;
-} pose_t;
+void set_noise(slambot_t *robot, double sensor_noise, double turn_noise, double motion_noise) {
+  robot->sensornoise = sensor_noise;
+  robot->turnnoise = turn_noise;
+  robot->motionnoise = motion_noise;
+}
 
-typedef struct odoup {
-  double drot1;
-  double dtrans;
-  double drot2;
-} odoup_t;
+// pretend there exists no noise
+void move(slambot_t *robot, double theta, double radius) {
+  robot->orientation.yaw += theta;
+  robot->orientation.x += cos(r->yaw) * radius;
+  robot->orientation.y += sin(r->yaw) * radius;
+}
 
-void odo_update(odoup_t *ou, pose_t s0, pose_t s1) {
-  double dy = s1.y - s0.y;
-  double dx = s1.x - s0.x;
-  ou->drot1 = atan2(dy, dx) - s0.theta;
-  ou->trans = sqrt(dy * dy + dx * dx);
-  ou->drot2 = s1.theta - (ou->trans + s0.theta);
+// sense the environment for landmarks
+void sense(slambot_t *robot) {
+  particle_resample(robot);
+}
+
+void particle_update(slambot_t *robot, pose3d_t *trajectory) {
+  int i;
+  for (i = 0; i < robot->numparticles; i++) {
+    robot->particles[i].x += trajectory.x +
+      gauss_error(0.0, robot->motionnoise);
+    robot->particles[i].y += trajectory.y +
+      gauss_error(0.0, robot->motionnoise);
+    robot->particles[i].yaw += trajectory.yaw +
+      gauss_error(0.0, robot->turnnoise);
+  }
+  robot->weights = calc_sqerr(robot->particles, world->landmarks);
+}
+
+// use the particle wheel (S. Thrun)
+// measurement update
+void particle_resample(slambot_t *robot) {
+  int i;
+  double wmax;
+  int index;
+  double beta;
+  double newparticles[robot->numparticles];
+  wmax = 0.0;
+  for (i = 0; i < robot->numweights; i++) {
+    if (robot->weights[i] > wmax) {
+      wmax = robot->weights[i];
+    }
+  }
+  beta = 0.0;
+  index = (int)(rand() * robot->numparticles) %
+    robot->numparticles;
+  for (i = 0; i < robot->numparticles; i++) {
+    // pick a random index
+    beta = beta + 2 * wmax;
+    for (; beta >= robot->particles[index];
+        index = (index + 1) % robot->numparticles) {
+      beta -= robot->particles[index];
+    }
+    newparticles[i] = robot->particles[index];
+  }
+  memcpy(robot->particles, newparticles, sizeof(newparticles));
+}
+
+double gauss_error(double mu, double sigma) {
+  double k, p;
+  k = 1.0 / sqrt(2 * PI * sigma * sigma);
+  p = (-(mu) / (2 * sigma * sigma));
+  return k * exp(p);
 }
