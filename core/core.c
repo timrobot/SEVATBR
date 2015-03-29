dc8f87c4615a5ae25bb9ce4190365d51de42e606
Timothy Yong <tyong_23@hotmail.com>
2015-03-29 05:34:06 -0400

    cleaned up core

--- core/core.c
+++ core/core.c
@@ -11,7 +11,6 @@
 #include <stdio.h>
 #include "robot.h"
 #include "manual.h"
-#include "agent.h"
 #include "coord.h"
 
 // Signal handler for killing the program
@@ -35,32 +34,21 @@ void stop_program(int signum) {
 int main(int argc, char *argv[]) {
   signal(SIGINT, stop_program);
   // init robot and manual
-  if (robot_set(TENNIS_BALL_ROBOT) == -1) { // TENNIS_BALL_ROBOT
+  if (robot_set(TENNIS_BALL_ROBOT) == -1) {
     return -1;
   }
-  manual_connect(MNL_SRVR); // not MNL_CTRL
-  //agent_create(AGENT_SIMPLE);
-
-  // change later
+  manual_connect(MNL_SRVR);
   manual_enable();
-  //agent_enable();
 
   // start getting communication accesses
   while (!stop_signal) {
     pose3d_t base;
     pose3d_t arm;
-    // choose input
-    //if (isOverriden()) {
-      manual_get_poses(&base, &arm);
-    //} else {
-      //agent_enable();
-      //agent_get_poses(&base, &arm);
-    //}
+    manual_get_poses(&base, &arm);
     robot_move(&base, &arm);
   }
 
   // clean up
-  //agent_destroy();
   manual_disconnect();
   robot_unset();
 
