06f76f7b9e2c00c5b0a19d6ddd07f4e8785f01c4
Timothy Yong <tyong_23@hotmail.com>
2015-04-29 23:36:02 -0400

    renabled festival

--- speech/speech.cpp
+++ speech/speech.cpp
@@ -1,5 +1,5 @@
 //#include "stt.h"
-//#include "tts.h"
+#include "tts.h"
 #include "speech.h"
 
 static char readbuf[128];
@@ -9,7 +9,6 @@ static char readbuf[128];
  */
 int speech::start(void) {
 /*  int res;
-  tts_select_voice("cmu_us_rms_cg");
   res = stt_start_listening();
   return res;*/
   return -1;
@@ -37,5 +36,5 @@ char *speech::listen(void) {
  */
 void speech::say(const char *fmt) {
   // TODO: account for var arg in later on
-//  tts_say(fmt);
+  tts_say(fmt);
 }
